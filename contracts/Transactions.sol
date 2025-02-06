ea2b4b86e3000bed84bf22759075fe2f55e1e80f
Niskarsh <niskarshk@gmail.com>
2025-02-06 16:31:18 +0530

    Update add fn

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -27,24 +27,47 @@ contract Transactions {
     Transaction[] public transactions;
 
     constructor() {
-        transactions.push(Transaction({
-            subject_entity_id: "nik",
-            subject_entity_type: EntityType.SmartContract,
-            action: "Init",
-            context_entity_id: "",
-            context_entity_type: EntityType.Null,
-            tokens_involved: "0",
-            token_price_in_inr: "token_price_in_inr",
-            created_at: "2025-02-06"
-
-        }));
+        transactions.push(
+            Transaction({
+                subject_entity_id: "nik",
+                subject_entity_type: EntityType.SmartContract,
+                action: "Init",
+                context_entity_id: "",
+                context_entity_type: EntityType.Null,
+                tokens_involved: "0",
+                token_price_in_inr: "0",
+                created_at: "2025-02-06"
+            })
+        );
     }
 
     function getAllTransactions() public view returns (Transaction[] memory) {
         return transactions;
     }
 
-    function addNewTransaction(Transaction memory newTransaction) public {
+    function addNewTransaction(
+        string memory _subject_entity_id,
+        EntityType _subject_entity_type,
+        string memory _action,
+        string memory _context_entity_id,
+        EntityType _context_entity_type,
+        string memory _tokens_involved,
+        string memory _token_price_in_inr,
+        string memory _created_at
+    ) public {
+        // Construct the Transaction struct from the input parameters
+        Transaction memory newTransaction = Transaction({
+            subject_entity_id: _subject_entity_id,
+            subject_entity_type: _subject_entity_type,
+            action: _action,
+            context_entity_id: _context_entity_id,
+            context_entity_type: _context_entity_type,
+            tokens_involved: _tokens_involved,
+            token_price_in_inr: _token_price_in_inr,
+            created_at: _created_at
+        });
+
+        // Store the new transaction in the transactions array
         transactions.push(newTransaction);
     }
 }
